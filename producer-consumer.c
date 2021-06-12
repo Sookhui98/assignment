10f23a9efae9b1dc491ce35d00eccdbc97b6642f
zeguan98 <85549212+zeguan98@users.noreply.github.com>
2021-06-12 18:15:43 +0800

    Try to implement the circular buffer_1
    The data and the buffer is declared in the form of array and the producer will take the data and put into the buffer while the consumer will remove the inserted data from the buffer. This can be seen through the watch window.

--- producer-consumer.c
+++ producer-consumer.c
@@ -8,15 +8,18 @@ osMessageQDef (Q_LED,0x16,unsigned char);
 osEvent  result;
 osMutexId uart_mutex;
 osMutexDef(uart_mutex);
-
+int size = 5;
+int data[5]={1,2,2,4,4};
+int clear[5] ={0,0,0,0,0};
+int buffer[5];
 void producer (void const *argument);																
 void consumer (void const *argument);
 int BUFFER_SIZE [4];
 int bufferlength = 0;
 int readpointer = 0;
 int writepointer = 0;
-int j;
-int i;
+int j,i;
+int z=0;
 osThreadDef(producer, osPriorityNormal, 1, 0);
 osThreadDef(consumer, osPriorityAboveNormal, 1, 0);
 osSemaphoreId sem1;									
@@ -31,12 +34,18 @@ void producer (void const *argument)
 	for (;;) 
 	{
 		osMutexWait(uart_mutex, osWaitForever);
-    for(i=1;i<=5;++i){
+		for(i=0;i<5;i++){
+				buffer[i]=data[i];
+        writepointer++;			
+		}
+
+ /*   for(i=1;i<=5;++i){
 			for (j=1;j<=i;++j){
+				buffer[z]=data[z];
 			  SendChar('*');		
 		}
 			SendChar('\n');
-	}
+	}*/
 		osSemaphoreRelease(sem1);
 	  osMutexRelease(uart_mutex);
 	   }
@@ -48,12 +57,17 @@ void consumer (void const *argument)
 	{
 		osSemaphoreWait(sem1, osWaitForever);
 		osMutexWait(uart_mutex, osWaitForever);
-		for (i=5; i>=1; --i){
+		for(i=0;i<5;i++){
+				buffer[i]=clear[i];	
+			  readpointer++;
+        			
+		}
+	/*for (i=5; i>=1; --i){
 			for (j=1; j<=i; ++j){
-				SendChar('*');
+				SendChar('i');			
 			}
 			SendChar('\n');
-		}
+		}*/
 		osMutexRelease(uart_mutex);
 	}
  }
@@ -70,4 +84,3 @@ int main (void)
 	
 	osKernelStart ();  
 }
-
