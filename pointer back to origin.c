2256f367dce3187c635389b07a3ee69b71b95926
Sookhui98 <85777160+Sookhui98@users.noreply.github.com>
2021-06-12 23:14:34 +0800

    add code to modify read and write pointer
    let read and write pointer back to origin (0) after complete the buffer cycle.

---
+++ pointer back to origin.c
@@ -0,0 +1,92 @@
+#include "stm32f10x.h"
+#include "cmsis_os.h"
+#include "uart.h"
+#include "Board_LED.h"
+
+osMessageQId Q_LED;																		//define the message queue
+osMessageQDef (Q_LED,0x16,unsigned char);
+osEvent  result;
+osMutexId uart_mutex;
+osMutexDef(uart_mutex);
+int size = 5;
+int data[5]={1,2,2,4,4};
+int clear[5] ={0,0,0,0,0};
+int buffer[5];
+void producer (void const *argument);																
+void consumer (void const *argument);
+int bufferlength = 0;
+int readpointer = 0;
+int writepointer = 0;
+int j,i;
+int z=0;
+osThreadDef(producer, osPriorityNormal, 1, 0);
+osThreadDef(consumer, osPriorityAboveNormal, 1, 0);
+osSemaphoreId sem1;									
+osSemaphoreDef(sem1);
+osThreadId T_uart1; //producer
+osThreadId T_uart2; //consumer
+osThreadId	T_main; 
+
+
+void producer (void const *argument) 
+{
+	for (;;) 
+	{
+		osMutexWait(uart_mutex, osWaitForever);
+		for(i=0;i<5;i++){
+				buffer[i]=data[i];
+        writepointer++;		
+        bufferlength++;	
+		}
+		if (writepointer==bufferlength){  
+			  writepointer=0;  ////let writepointer back to origin
+			}
+
+ /*   for(i=1;i<=5;++i){
+			for (j=1;j<=i;++j){
+				buffer[z]=data[z];
+			  SendChar('*');		
+		}
+			SendChar('\n');
+	}*/
+		osSemaphoreRelease(sem1);
+	  osMutexRelease(uart_mutex);
+	   }
+  }
+
+void consumer (void const *argument) 
+{
+	for (;;) 
+	{
+		osSemaphoreWait(sem1, osWaitForever);
+		osMutexWait(uart_mutex, osWaitForever);
+		for(i=0;i<5;i++){
+				buffer[i]=clear[i];	
+			  readpointer++;
+        bufferlength--;			
+		}
+		if (bufferlength==0){
+		readpointer=0;       //let readpointer back to origin 
+		}
+	/*for (i=5; i>=1; --i){
+			for (j=1; j<=i; ++j){
+				SendChar('i');			
+			}
+			SendChar('\n');
+		}*/
+		osMutexRelease(uart_mutex);
+	}
+ }
+
+int main (void) 
+{
+	osKernelInitialize (); 	// initialize CMSIS-RTOS
+	
+	USART1_Init ();
+	uart_mutex = osMutexCreate(osMutex(uart_mutex));					//create the message queue
+	T_uart1 = osThreadCreate(osThread(producer), NULL);
+	T_uart2 =	osThreadCreate(osThread(consumer), NULL);
+	sem1 = osSemaphoreCreate(osSemaphore(sem1), 0);	
+	
+	osKernelStart ();  
+}
