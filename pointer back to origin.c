374fd642fbdd91f796ba6107fbac251e48717e56
zeguan98 <85549212+zeguan98@users.noreply.github.com>
2021-06-18 20:31:40 +0800

    Remove for loop and creating semaphore
    The for loop for placing and reading data is removed and the semaphore name inserted_item is created to make sure the consumer will not run if the producer haven't do so.

--- pointer back to origin.c
+++ pointer back to origin.c
@@ -5,23 +5,25 @@
 osMutexId uart_mutex; 
 osMutexDef(uart_mutex);
 const unsigned int size = 5;
-int data[5]={1,2,2,4,4};
-int clear[5] ={0,0,0,0,0};
 int buffer[size];
+
 void producer (void const *argument);																
 void consumer (void const *argument);
-int bufferlength = 0;
+
 int readpointer = 0;
 int writepointer = 0;
 int i;
 
 osThreadDef(producer, osPriorityNormal, 1, 0);
-osThreadDef(consumer, osPriorityAboveNormal, 1, 0);
-osSemaphoreId sem1;									
-osSemaphoreDef(sem1);
+osThreadDef(consumer, osPriorityNormal, 1, 0);
+
+osSemaphoreId inserted_item;									
+osSemaphoreDef(inserted_item);
+
+
 osThreadId T_uart1; //producer
 osThreadId T_uart2; //consumer
-osThreadId T_main; 
+
 
 
 void producer (void const *argument) 
@@ -29,13 +31,11 @@ void producer (void const *argument)
 	for (;;) 
 	{
 		osMutexWait(uart_mutex, osWaitForever);
-		for(i=0;i<5;i++){
 		buffer[i]=data[i];
-                writepointer = (writepointer+1) % size;	  //return writepointer to 0 after it reached 5		
-	}
-
-		osSemaphoreRelease(sem1);
+		writepointer=(writepointer+1)%size;	
 	        osMutexRelease(uart_mutex);
+		osSemaphoreRelease(inserted_item);
+
 	   }
   }
 
@@ -43,13 +43,10 @@ void consumer (void const *argument)
 {
 	for (;;) 
 	{
-		osSemaphoreWait(sem1, osWaitForever);
+		osSemaphoreWait(inserted_item, osWaitForever);
 		osMutexWait(uart_mutex, osWaitForever);
-		for(i=0;i<5;i++){
 		buffer[i]=clear[i];	
-	        readpointer = (readpointer+1) % size;	  //return readpointer to 0 after it reached 5	
-         }
-		
+		readpointer=(readpointer+1)%size;	
 		osMutexRelease(uart_mutex);
 	}
  }
@@ -57,12 +54,10 @@ void consumer (void const *argument)
 int main (void) 
 {
 	osKernelInitialize (); 	// initialize CMSIS-RTOS
-	
 	USART1_Init ();
-	uart_mutex = osMutexCreate(osMutex(uart_mutex));	//create the message queue
+	uart_mutex = osMutexCreate(osMutex(uart_mutex));					//create the message queue
 	T_uart1 = osThreadCreate(osThread(producer), NULL);
-	T_uart2 = osThreadCreate(osThread(consumer), NULL);
-	sem1 = osSemaphoreCreate(osSemaphore(sem1), 0);	       //token initialised in sem1 = 0
-	
+	T_uart2 =	osThreadCreate(osThread(consumer), NULL);
+	inserted_item = osSemaphoreCreate(osSemaphore(inserted_item), 0);
 	osKernelStart ();  
 }
