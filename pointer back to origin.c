be122a785157e14a3c4e75c1e130de8e4b99f77e
zeguan98 <85549212+zeguan98@users.noreply.github.com>
2021-06-19 17:35:20 +0800

    Adding testing code

--- pointer back to origin.c
+++ pointer back to origin.c
@@ -2,18 +2,20 @@
 #include "cmsis_os.h"
 #include "uart.h"
 
-osMutexId uart_mutex; 
+
+osMutexId uart_mutex;
 osMutexDef(uart_mutex);
+
 const unsigned int size = 5;
+
 unsigned char buffer[size];
-
-
 void producer (void const *argument);																
 void consumer (void const *argument);
 
-unsigned int readpointer = 0;
 unsigned int writepointer = 0;
-unsigned int value = 0X30;
+unsigned int readpointer = 0;
+unsigned int variable = 0x30;
+//unsigned int variable = 0x41;  Testing code, the variable become alphabet instead of integer
 unsigned int data = 0x00;
 
 
@@ -24,11 +26,8 @@ osSemaphoreId inserted_item;
 osSemaphoreDef(inserted_item);
 osSemaphoreId space;									
 osSemaphoreDef(space);
-
 osThreadId T_uart1; //producer
-osThreadId T_uart2; //consumer
-
-
+osThreadId T_uart2; //consumer 
 
 void producer (void const *argument) 
 {
@@ -36,15 +35,14 @@ void producer (void const *argument)
 	{
 		osSemaphoreWait(space, osWaitForever);  
 		osMutexWait(uart_mutex, osWaitForever);
-		buffer[writepointer]=value;
-		value++
-		writepointer=(writepointer+1)%size;	
-	        osMutexRelease(uart_mutex);
-		osSemaphoreRelease(inserted_item);
-		if (value == 0x3A){
-			value = 0x30;
-		}
-
+		buffer[writepointer]=variable;
+		variable++;
+   		writepointer=(writepointer+1)%size;		
+		osMutexRelease(uart_mutex);
+		osSemaphoreRelease(inserted_item); //item inserted signal
+	  if (variable == 0x3A){
+				variable = 0x30;
+				}      
 	   }
   }
 
@@ -55,24 +53,27 @@ void consumer (void const *argument)
 		osSemaphoreWait(inserted_item, osWaitForever);
 		osMutexWait(uart_mutex, osWaitForever);
 		data=buffer[readpointer];	
-		readpointer=(readpointer+1)%size;	
+		readpointer=(readpointer+1)%size;		
 		osMutexRelease(uart_mutex);
 		osSemaphoreRelease(space);
-		if (readpointer % 5) == 0){
-			SendChar ('\n");
-		}
-
+		SendChar(data);
+		if ((readpointer%5) == 0){
+			SendChar('\n');
+			}
+		
 	}
- }
+
+}
 
 int main (void) 
 {
 	osKernelInitialize (); 	// initialize CMSIS-RTOS
+	
 	USART1_Init ();
 	uart_mutex = osMutexCreate(osMutex(uart_mutex));					//create the message queue
 	T_uart1 = osThreadCreate(osThread(producer), NULL);
 	T_uart2 = osThreadCreate(osThread(consumer), NULL);
-	inserted_item = osSemaphoreCreate(osSemaphore(inserted_item), 0);
+	inserted_item = osSemaphoreCreate(osSemaphore(inserted_item), 0);	
 	space = osSemaphoreCreate(osSemaphore(space), size);
 	osKernelStart ();  
 }
